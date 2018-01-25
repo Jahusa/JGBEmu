a6ab1ee6cc4a6c71668d9aba2ca6cd29d2d310fe
Emir Hodzic <emir.hodzic@gmx.at>
2018-01-26 00:22:59 +0100

    added initial method for loading roms into switchable banks

--- src/main/java/com/jgbemu/core/memory/ROM.java
+++ src/main/java/com/jgbemu/core/memory/ROM.java
@@ -1,14 +1,13 @@
 package com.jgbemu.core.memory;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 
 public class ROM implements Memory{
-	//private byte[] romBank0;
-	//private byte[] romBankSwitchable;
-	private final short offset = 0x0000;
-	private byte[] romBanks;
+	private byte[][] romBanks;
 	private byte[] rom;
 
 	public ROM(String romPath) {
@@ -38,6 +37,41 @@ public class ROM implements Memory{
 	public byte[] getRamStack() {
 		//TODO: ROM-Bank
 		return rom;
+	}
+
+	public static byte[][] getBytesFromFile(File file) throws IOException {
+		InputStream is = new FileInputStream(file);
+		long length = file.length();
+
+		int bankSize = 128*128;
+		int numberOfBanks = (int) (length/bankSize);
+
+		// Create the byte array to hold the data
+		byte[][] temp = new byte[numberOfBanks][bankSize];
+
+		// Read in the bytes
+		for (int i = 0; i < numberOfBanks; i++) {
+
+            System.out.println("Bank: " + i);
+            int offset = i*bankSize;
+            int numRead = 0;
+            byte[] tempBank = new byte[bankSize];
+            while (offset < (tempBank.length+offset) && (numRead = is.read(tempBank, (offset-1), tempBank.length)) >= 0) {
+                offset += numRead;
+            }
+
+            // Ensure all the bytes have been read in
+            if (offset < temp.length) {
+                throw new IOException("Could not completely read file "+file.getName());
+            }
+
+
+
+            temp[i] = tempBank;
+        }
+
+		is.close();
+		return temp;
 	}
 
 	public void switchBank(byte bank) {
